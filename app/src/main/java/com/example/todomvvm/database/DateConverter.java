f35d93206bbc93680eb8ca3f6d29879823948c65
binay-shah <binay18.shah@gmail.com>
2020-04-19 09:20:44 +0545

    step.04-Exercise-Create-TypeConverter

---
+++ app/src/main/java/com/example/todomvvm/database/DateConverter.java
@@ -0,0 +1,14 @@
+package com.example.todomvvm.database;
+
+import androidx.room.TypeConverter;
+
+import java.util.Date;
+
+public class DateConverter {
+
+    //Todo(1) Create annotated method toDate()
+
+    //Todo(2) Create annotated method toTimeStamp
+}
+
+
