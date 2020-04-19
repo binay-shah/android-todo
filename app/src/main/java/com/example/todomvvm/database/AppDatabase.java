5d2970dc17ac8856018bfc11066615484f78a4dc
binay-shah <binay18.shah@gmail.com>
2020-04-19 09:11:42 +0545

    step.03-Exercise-Create-Database

---
+++ app/src/main/java/com/example/todomvvm/database/AppDatabase.java
@@ -0,0 +1,13 @@
+package com.example.todomvvm.database;
+
+// TODO (01) Create an abstract class that extends RoomDatabase.
+
+// TODO (02) Declare an abstract value of type TaskDao.
+
+// TODO (04) Declare a @Volatile sINSTANCE variable.
+
+// TODO (05) Define a getInstance() method with a synchronized block.
+
+// TODO (06) Inside the synchronized block:
+// Check whether the database already exists,
+// and if it does not, use Room.databaseBuilder to create it.
